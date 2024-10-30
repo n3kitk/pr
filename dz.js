601e69c4216e22f11a67c40d4bbe190c61876ace
Nikita2007uu <148580169+nkknickk@users.noreply.github.com>
2024-10-30 20:24:35 +0300

    main

---
+++ dz.js
@@ -0,0 +1,54 @@
+const Celsies = 29;
+const Farengeits = Celsies * 1.8 + 32;
+console.log (Farengeits);
+
+const days = 31;
+const hours = 31*24;
+const minutes = 31*1440;
+console.log (hours);
+console.log (minutes);
+
+
+const health = 100;
+const energy = 50;
+const newhealth = 100 - 10;
+const newenergy = 50-20;
+console.log (newhealth);
+console.log (newenergy);
+
+
+const purchaseAmount = 1000; 
+const discountAmount = purchaseAmount * 0.9; 
+console.log(discountAmount); 
+
+
+
+const floatNumber = 7.56; 
+const roundedNumber = Math.floor(floatNumber); 
+console.log(roundedNumber); 
+
+
+const Number = 10.56;
+const result = parseFloat(Number);
+console.log (result);
+
+
+const a = 15;
+b = parseInt(a);
+console.log(b)
+
+
+const c = 256;
+const e = Math.sqrt(c);
+console.log (e);
+
+
+
+
+const numberString = "42";
+
+const integerValue = parseInt(numberString);
+console.log(integerValue);
+
+const stringValue = integerValue.toString();
+console.log(stringValue); 
